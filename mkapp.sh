a38ae78d1152e020daed13482a7e9757492ad3da
Mr. Pennyworth <mr-pennyworth@users.noreply.github.com>
2020-11-24 17:47:26 +0530

    Use python3 explicitly in build scripts

--- mkapp.sh
+++ mkapp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd pyapp
-pip install -r requirements.txt
-python BetterDict.setup.py py2app --dist-dir=../ $@
+pip3 install -r requirements.txt
+python3 BetterDict.setup.py py2app --dist-dir=../ $@
 cd ..
