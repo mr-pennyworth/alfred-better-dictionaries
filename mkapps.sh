b30b4c3b0fd11fb4ef949a7ee07455a01762a1d1
Mr. Pennyworth <mr-pennyworth@users.noreply.github.com>
2024-02-10 06:11:38 -0600

    Download meilisearch binaries while building
    GitHub has disabled git LFS as it has exceeded the quota.
    Thus, checking in the binaries is no longer an option.

--- mkapps.sh
+++ mkapps.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 set -o xtrace
 
-# Pick the correct meilisearch binary for the arch
-git lfs pull
-cp "meilisearch-$(uname -m)" alfred-dict-server
+ARCH=$(uname -m)
+
+MEILISEARCH_RELEASE_URL="https://github.com/meilisearch/meilisearch/releases"
+URL_INTEL="$MEILISEARCH_RELEASE_URL/download/v1.6.1/meilisearch-macos-amd64"
+URL_APPLE_SILICON="$MEILISEARCH_RELEASE_URL/download/v1.6.1/meilisearch-macos-apple-silicon"
+
+if [ "$ARCH" = "x86_64" ]; then
+  curl -L $URL_INTEL -o alfred-dict-server
+else
+  curl -L $URL_APPLE_SILICON -o alfred-dict-server
+fi
+
 
 # Build the python scripts into a standalone binary
 pip3 install -r pyapp/requirements.txt
