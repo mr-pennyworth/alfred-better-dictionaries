f0ea85ce17b01a1c99e4cee0ebca19bd0b176c72
Mr. Pennyworth <mr-pennyworth@users.noreply.github.com>
2024-02-10 06:51:12 -0600

    Fix Apple Silicon builds by including correct jq binary

--- mkapps.sh
+++ mkapps.sh
@@ -4,14 +4,23 @@ set -o xtrace
 ARCH=$(uname -m)
 
 MEILISEARCH_RELEASE_URL="https://github.com/meilisearch/meilisearch/releases"
-URL_INTEL="$MEILISEARCH_RELEASE_URL/download/v1.6.1/meilisearch-macos-amd64"
-URL_APPLE_SILICON="$MEILISEARCH_RELEASE_URL/download/v1.6.1/meilisearch-macos-apple-silicon"
+MS_INTEL="$MEILISEARCH_RELEASE_URL/download/v1.6.1/meilisearch-macos-amd64"
+MS_APPLE="$MEILISEARCH_RELEASE_URL/download/v1.6.1/meilisearch-macos-apple-silicon"
+
+JQ_RELEASE_URL="https://github.com/jqlang/jq/releases"
+JQ_INTEL="$JQ_RELEASE_URL/download/jq-1.7.1/jq-macos-amd64"
+JQ_APPLE="$JQ_RELEASE_URL/download/jq-1.7.1/jq-macos-arm64"
 
 if [ "$ARCH" = "x86_64" ]; then
-  curl -L $URL_INTEL -o alfred-dict-server
+  curl -L $MS_INTEL -o alfred-dict-server
+  curl -L $JQ_INTEL -o jq
 else
-  curl -L $URL_APPLE_SILICON -o alfred-dict-server
+  curl -L $MS_APPLE -o alfred-dict-server
+  curl -L $JQ_APPLE -o jq
 fi
+
+chmod +x alfred-dict-server
+chmod +x jq
 
 
 # Build the python scripts into a standalone binary
